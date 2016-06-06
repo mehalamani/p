356b4cf0c54ff54ed643129d68d78905db9e3339
mehalamani <Smehala6789@gmail.com>
2016-06-06 20:26:15 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,13 @@
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+		printf("Given number is zero\n");
+	else if(n>0)
+		printf("Given number is positive\n");
+	else
+		printf("Given number is negative\n");
+	// your code goes here
+	return 0;
+}
+ 
